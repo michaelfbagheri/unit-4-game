6ae56ecc89a1bb41819ac706023b74e0d9df2c90
mb480s <mike.f.bagheri@gmail.com>
2018-09-13 10:06:55 -0400

    HTML and mechanics working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,196 +1,261 @@
 $(document).ready(function(){
-//debugger;
-
-var president1 = {
-    alreadyPicked: false,
-    name: 'Clinton',
-    baseAttackPower:10,
-    baseAttackPowerConst: 10,
-    counterAttackPower: 5,
-    baseHealth: 50,
-    numOfAttacks: 0,
-    order: '',
+    //debugger;
     
-};
+    var slinger1 = {
+        alreadyPicked: false,
+        name: 'Billy The Kid',
+        baseAttackPower:10,
+        baseAttackPowerConst: 10,
+        counterAttackPower: 5,
+        baseHealth: 50,
+        numOfAttacks: 0,
+        order: '',
+        
+    };
+    
+    var slinger2 = {
+        alreadyPicked: false,
+        name: 'Pearl Heart',
+        baseAttackPower:7,
+        baseAttackPowerConst: 7,
+        counterAttackPower: 5,
+        baseHealth: 50,
+        numOfAttacks: 0,
+        order: '',
+    };
+    
+    var slinger3 = {
+        alreadyPicked: false,
+        name: 'Doc Holliday',
+        baseAttackPower:5,
+        baseAttackPowerConst: 5,
+        counterAttackPower: 5,
+        baseHealth: 50,
+        numOfAttacks: 0,
+        order: '',
+    };
+    
+    var slinger4 = {
+        alreadyPicked: false,
+        name: 'Wyatt Earp',
+        baseAttackPower:5,
+        baseAttackPowerConst: 5,
+        counterAttackPower: 5,
+        baseHealth: 50,
+        numOfAttacks: 0,
+        order: '',
+    };
 
-var president2 = {
-    alreadyPicked: false,
-    name: 'Bush',
-    baseAttackPower:7,
-    baseAttackPowerConst: 7,
-    counterAttackPower: 5,
-    baseHealth: 50,
-    numOfAttacks: 0,
-    order: '',
-};
-
-var president3 = {
-    alreadyPicked: false,
-    name: 'Carter',
-    baseAttackPower:5,
-    baseAttackPowerConst: 5,
-    counterAttackPower: 5,
-    baseHealth: 50,
-    numOfAttacks: 0,
-    order: '',
-};
-
-var opponentPicked = true;
-
-var player = $('#player')
-var computer = $('#computer')
-var paragraph1 = $('<p>')
-var paragraph2 = $('<p>')
-var paragraph3 = $('<p>')
-var paragraph4 = $('<p>')
-var paragraph5 = $('<p>')
-var paragraph6 = $('<p>')
-
-$('#pres1').append(president1.name)
-$('#pres2').append(president2.name)
-$('#pres3').append(president3.name)
-
-
-$('#pres1').on('click',function() {
-    if (!president1.alreadyPicked && !president2.alreadyPicked && !president3.alreadyPicked){        
-        player.text(president1.name)
-        $('#pres1').empty();
-        $('#pres1').append('Away to WAR!!')
-        $('#player').append(paragraph1);
-        $('#player').append(paragraph2);
-        paragraph1.attr('Id','playerattackpwr');
-        paragraph2.attr('id', 'playerbasehealth');
-        $('#playerattackpwr').text('Base attack power: ' + president2.baseAttackPower);
-        $('#playerbasehealth').text('Base health: ' + president2.baseHealth);
-        $('#pres1').off('click')
-        president1.alreadyPicked = true;
-        president1.order = 'mainPlayer';
-    }
-    else {
-        var computerOne = president1;
-        computer.text(computerOne.name)
-        $('#computer').append(paragraph4);
-        paragraph4.attr('Id','computerBaseHealth');
-        $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
-        $('#pres1').off('click');
-        president1.alreadyPicked = true;
-        opponentPicked = false;
-        president1.order = 'opponent'
-    }
-
-});
-
-$('#pres2').on('click',function() {
-    debugger;
-    if (!president1.alreadyPicked && !president2.alreadyPicked && !president3.alreadyPicked){        
-        player.text(president2.name)
-        $('#pres2').empty();
-        $('#pres2').append('Away to WAR!!')
-        $('#player').append(paragraph1);
-        $('#player').append(paragraph2);
-        paragraph1.attr('Id','playerattackpwr');
-        paragraph2.attr('id', 'playerbasehealth');
-        $('#playerattackpwr').text('Base attack power: ' + president2.baseAttackPower);
-        $('#playerbasehealth').text('Base health: ' + president2.baseHealth);
-        $('#pres2').off('click')
-        president2.alreadyPicked = true;
-        president2.order = 'mainPlayer';
-    }
-    else {
-        var computerOne = president2;
-        computer.text(computerOne.name)
-        $('#computer').append(paragraph4);
-        paragraph4.attr('Id','computerBaseHealth');
-        $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
-        $('#pres2').off('click');
-        president2.alreadyPicked = true;
-        opponentPicked = false;
-        president2.order = 'opponent';
-    }
-});
-
-
-    $('#pres3').on('click',function() {
-        if (!president1.alreadyPicked && !president2.alreadyPicked && !president3.alreadyPicked){        
-            player.text(president3.name)
-            $('#pres3').empty();
-            $('#pres3').append('Away to WAR!!')
+    var opponentPicked = true;
+    
+    var player = $('#player')
+    var computer = $('#computer')
+    var paragraph1 = $('<p>')
+    var paragraph2 = $('<p>')
+    var paragraph3 = $('<p>')
+    var paragraph4 = $('<p>')
+    var paragraph5 = $('<p>')
+    var paragraph6 = $('<p>')
+    var paragraph7 = $('<p>')
+    var paragraph8 = $('<p>')
+    var newImgPlayer = $('<img>')
+    var newImgComputer = $('<img>')
+    var newDiv = $('<div>')
+    
+    //$('#slinger1').append(slinger1.name)
+    //$('#slinger2').append(slinger2.name)
+    //$('#slinger3').append(slinger3.name)
+    
+    
+    $('#slinger1').on('click',function() {
+        if (!slinger1.alreadyPicked && !slinger2.alreadyPicked && !slinger3.alreadyPicked){        
+            player.text(slinger1.name)
+            $('#slinger1').empty();
+            $('#slinger1').append('Away to WAR!!')
             $('#player').append(paragraph1);
+            paragraph1.addClass('test');
             $('#player').append(paragraph2);
             paragraph1.attr('Id','playerattackpwr');
             paragraph2.attr('id', 'playerbasehealth');
-            $('#playerattackpwr').text('Base attack power: ' + president3.baseAttackPower);
-            $('#playerbasehealth').text('Base health: ' + president3.baseHealth);
-            $('#pres3').off('click')
-            president3.alreadyPicked = true;
-            president3.order = 'mainPlayer';
+            $('#playerattackpwr').text('Base attack power: ' + slinger2.baseAttackPower);
+            $('#playerbasehealth').text('Base health: ' + slinger2.baseHealth);
+            $('#slinger1').off('click')
+            slinger1.alreadyPicked = true;
+            slinger1.order = 'mainPlayer';
+            $('#player').attr('style',"background-image:url('assets/image/BillyTheKid150X125.jpg');")
+            $('.goodGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
         }
         else {
-            var computerOne = president3;
+            var computerOne = slinger1;
             computer.text(computerOne.name)
             $('#computer').append(paragraph4);
             paragraph4.attr('Id','computerBaseHealth');
             $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
-            $('#pres3').off('click');
-            president3.alreadyPicked = true;
+            $('#slinger1').off('click');
+            slinger1.alreadyPicked = true;
             opponentPicked = false;
-            president3.order = 'opponent';
+            slinger1.order = 'opponent'
+            $('#computer').attr('style',"background-image:url('assets/image/BillyTheKid150X125.jpg');")
+            $('.badGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
         }
     
-});
-
-
-
-
-
-
-
-player.on('click',function() {
-    if (!opponentPicked){
-        switch('mainPlayer'){
-            case president1.order:
-            var playerOne = president1;
-            break;
-            case president2.order:
-            var playerOne = president2;
-            break;
-            case president3.order:
-            var playerOne = president3;
-            break;
+    });
+    
+    $('#slinger2').on('click',function() {
+        if (!slinger1.alreadyPicked && !slinger2.alreadyPicked && !slinger3.alreadyPicked){        
+            player.text(slinger2.name)
+            $('#slinger2').empty();
+            $('#slinger2').append('Away to WAR!!')
+            $('#player').append(paragraph1);
+            $('#player').append(paragraph2);
+            paragraph1.attr('Id','playerattackpwr');
+            paragraph2.attr('id', 'playerbasehealth');
+            $('#playerattackpwr').text('Base attack power: ' + slinger2.baseAttackPower);
+            $('#playerbasehealth').text('Base health: ' + slinger2.baseHealth);
+            $('#slinger2').off('click')
+            slinger2.alreadyPicked = true;
+            slinger2.order = 'mainPlayer';
+            $('#player').attr('style',"background-image:url('assets/image/PearlHeart.jpg');")
+            $('.goodGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
         }
-
-        switch('opponent'){
-            case president1.order:
-            var computerOne = president1;
-            break;
-            case president2.order:
-            var computerOne = president2;
-            break;
-            case president3.order:
-            var computerOne = president3;
-            break;
+        else {
+            var computerOne = slinger2;
+            computer.text(computerOne.name)
+            $('#computer').append(paragraph4);
+            paragraph4.attr('Id','computerBaseHealth');
+            $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
+            $('#slinger2').off('click');
+            slinger2.alreadyPicked = true;
+            opponentPicked = false;
+            slinger2.order = 'opponent';
+            $('#computer').attr('style',"background-image:url('assets/image/PearlHeart.jpg');")
+            $('.badGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
         }
+    });
+    
+    
+        $('#slinger3').on('click',function() {
+            if (!slinger1.alreadyPicked && !slinger2.alreadyPicked && !slinger3.alreadyPicked){        
+                player.text(slinger3.name)
+                $('#slinger3').empty();
+                $('#slinger3').append('Away to WAR!!')
+                $('#player').append(paragraph1);
+                $('#player').append(paragraph2);
+                paragraph1.attr('Id','playerattackpwr');
+                paragraph2.attr('id', 'playerbasehealth');
+                $('#playerattackpwr').text('Base attack power: ' + slinger3.baseAttackPower);
+                $('#playerbasehealth').text('Base health: ' + slinger3.baseHealth);
+                $('#slinger3').off('click')
+                slinger3.alreadyPicked = true;
+                slinger3.order = 'mainPlayer';
+                $('#player').attr('style',"background-image:url('assets/image/DocHolliday.jpg');")
+                $('.goodGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
+            }
+            else {
+                var computerOne = slinger3;
+                computer.text(computerOne.name)
+                $('#computer').append(paragraph4);
+                paragraph4.attr('Id','computerBaseHealth');
+                $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
+                $('#slinger3').off('click');
+                slinger3.alreadyPicked = true;
+                opponentPicked = false;
+                slinger3.order = 'opponent';
+                $('#computer').attr('style',"background-image:url('assets/image/DocHolliday.jpg');")
+                $('.badGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
+            }
+
+        });
+
+            $('#slinger4').on('click',function() {
+                if (!slinger1.alreadyPicked && !slinger2.alreadyPicked && !slinger3.alreadyPicked && !slinger4.alreadyPicked){        
+                    player.text(slinger4.name)
+                    $('#slinger4').empty();
+                    $('#slinger4').append('Away to WAR!!')
+                    $('#player').append(paragraph1);
+                    $('#player').append(paragraph2);
+                    paragraph1.attr('Id','playerattackpwr');
+                    paragraph2.attr('id', 'playerbasehealth');
+                    $('#playerattackpwr').text('Base attack power: ' + slinger4.baseAttackPower);
+                    $('#playerbasehealth').text('Base health: ' + slinger4.baseHealth);
+                    $('#slinger4').off('click')
+                    slinger4.alreadyPicked = true;
+                    slinger4.order = 'mainPlayer';
+                    $('#player').attr('style',"background-image:url('assets/image/WyattEarp150X125.png');")
+                    $('.goodGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
+                }
+                else {
+                    var computerOne = slinger4;
+                    computer.text(computerOne.name)
+                    $('#computer').append(paragraph4);
+                    paragraph4.attr('Id','computerBaseHealth');
+                    $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
+                    $('#slinger4').off('click');
+                    slinger4.alreadyPicked = true;
+                    opponentPicked = false;
+                    slinger4.order = 'opponent';
+                    $('#computer').attr('style',"background-image:url('assets/image/WyattEarp150X125.png');")
+                    $('.badGuy').css({"border":"black 3px solid", "-webkit-text-stroke": "1px #f0ec08","-webkit-filter": "drop-shadow(2px 2px 20px gold)"})
+                }
         
-        //playerOne.baseAttackPower = playerOne.baseAttackPower + playerOne.baseAttackPowerConst;   
-        computerOne.baseHealth = computerOne.baseHealth - playerOne.baseAttackPower;
-        playerOne.baseHealth = playerOne.baseHealth - computerOne.counterAttackPower;
-        $('#computerBaseHealth').text('President ' + computerOne.name + ' base health: ' + computerOne.baseHealth);
-        playerOne.baseAttackPower = playerOne.baseAttackPower + playerOne.baseAttackPowerConst;
-        $('#playerattackpwr').text('President ' + playerOne.name + ' base attack power: ' + playerOne.baseAttackPower);
-        $('#playerbasehealth').text('President ' + playerOne.name + ' base health: ' + playerOne.baseHealth);
-        if (computerOne.baseHealth < 0){
-            $('#computerBaseHealth').text('President ' + computerOne.name + ' has been defeated!');
-            opponentPicked = true;
+    });
+    
+    
+    
+    
+    
+    
+    
+    player.on('click',function() {
+        if (!opponentPicked){
+            switch('mainPlayer'){
+                case slinger1.order:
+                var playerOne = slinger1;
+                break;
+                case slinger2.order:
+                var playerOne = slinger2;
+                break;
+                case slinger3.order:
+                var playerOne = slinger3;
+                break;
+                case slinger4.order:
+                var playerOne = slinger4;
+            }
+    
+            switch('opponent'){
+                case slinger1.order:
+                var computerOne = slinger1;
+                break;
+                case slinger2.order:
+                var computerOne = slinger2;
+                break;
+                case slinger3.order:
+                var computerOne = slinger3;
+                break;
+                case slinger4.order:
+                var computerOne = slinger4;
+            }
+            
+            //playerOne.baseAttackPower = playerOne.baseAttackPower + playerOne.baseAttackPowerConst;   
+            computerOne.baseHealth = computerOne.baseHealth - playerOne.baseAttackPower;
+            playerOne.baseHealth = playerOne.baseHealth - computerOne.counterAttackPower;
+            $('#computerBaseHealth').text('Base health: ' + computerOne.baseHealth);
+            playerOne.baseAttackPower = playerOne.baseAttackPower + playerOne.baseAttackPowerConst;
+            $('#playerattackpwr').text('Attack power: ' + playerOne.baseAttackPower);
+            $('#playerbasehealth').text('Base health: ' + playerOne.baseHealth);
+            if (computerOne.baseHealth < 0){
+                $('#computerBaseHealth').text(computerOne.name + ' has been defeated!');
+                opponentPicked = true;
+            }
+    
+            if (playerOne.baseHealth < 0){
+                $('#playersbasehealth').text(playerOne.name + ' has been defeated!');
+                opponentPicked = true;
+            }
         }
-
-        if (playerOne.baseHealth < 0){
-            $('#playersbasehealth').text('President ' + playerOne.name + ' has been defeated!');
-            opponentPicked = true;
-        }
-    }
-});
-
-
-
-
-});
+    });
+    
+    
+    
+    
+    });
